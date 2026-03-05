97cf6d960c15603a64f86a3919347a232204ac2c
Rula <duraidirula@gmail.com>
2026-03-05 04:20:21 +0200

    feat(contact section):implement contact section

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hero from './component/Hero/Hero.jsx'
+import ContactSection from './component/ContactUs/ContactUs.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hero />
+    <ContactSection/>
   </StrictMode>,
 )
